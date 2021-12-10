cb79bab38955d5b9d0aefb589f4cffbd244fe29b
5034kai <86808516+5034kai@users.noreply.github.com>
2021-12-10 11:17:55 +0900

    Delete src directory

--- src/main/java/com/example/demo/service/MemberService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import java.util.List;
-import com.example.demo.entity.Member;
-
-public interface MemberService {
-    String greet(int i);
-    List<Member> getAll();
-}
